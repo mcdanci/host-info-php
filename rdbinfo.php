a2debe1fdce0f3b7b8fcb11abd25436aa1a99087
McDanci <mcdanci@users.noreply.github.com>
2016-11-11 16:51:26 +0800

    Add rdbinfo module

---
+++ rdbinfo.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Date: 11/11/2016
+ * Time: 15:16
+ *
+ * 說明：當前手稿會佔用 1 thread 同 1 question。
+ */
+include './rdbinfo_conf.php';
+
+function di_mysql_stat($host, $user, $password, $dbName)
+{
+    $stat = null;
+
+    $link = new mysqli($host, $user, $password, $dbName);
+    if (mysqli_connect_errno()) {
+        return array(mysqli_connect_error());
+    } else {
+        $result = explode('  ', $link->stat());
+        var_dump($link->stat()); // todo: debug
+        foreach ($result as $k => $v) {
+            list($subK, $subV) = explode(': ', $v);
+            $stat[$subK] = $subV;
+        }
+
+        return [
+            'status' => $stat,
+            'server info' => $link->server_info,
+            'protocol version' => $link->protocol_version,
+            'server version' => $link->server_version,
+        ];
+    }
+}
+
+// main process
+echo json_encode(di_mysql_stat(DB_HOST, DB_USER, DB_USERPASSWD, DB_NAME));
---
+++ rdbinfo_conf.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Configuration.
+ * Date: 11/11/2016
+ * Time: 15:51
+ */
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_USERPASSWD', '');
+define('DB_NAME', '');
